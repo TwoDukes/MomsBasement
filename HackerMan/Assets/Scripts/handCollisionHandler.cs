2396c26889194d2925d8fc409b3b67445b24d05f
magicdustin <magicdustin14@gmail.com>
2016-03-01 14:51:26 -0800

    fixed hand collision issue and hand trigger bounds are more realistic

---
+++ HackerMan/Assets/Scripts/handCollisionHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handCollisionHandler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Physics.IgnoreLayerCollision(8, 9, true);
+    }
+}
