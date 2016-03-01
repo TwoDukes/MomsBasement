1299e0f8616dda47bd26eeced100415eff52b7e9
magicdustin <magicdustin14@gmail.com>
2016-03-01 01:05:01 -0800

    added initial working set of hand control scripts!
    Throw cubes to your hearts content
    
    Known fixes still needed:
    *Collisions with RB objects connected to the world need to be disabled
    *Delay till hand comes on needs to become a dynamic system
    *Fix large Trigger Bounds for picking up objects

--- HackerMan/Assets/Scripts/HandHandler.cs
+++ HackerMan/Assets/Scripts/HandHandler.cs
@@ -5,10 +5,12 @@ using System.Collections;
 public class HandHandler : MonoBehaviour
 {
 
-    public Rigidbody attachPoint;
+    public GameObject attachObject, handModel;
+    public float pullPower;
 
+    Rigidbody attachPoint;
     GameObject CurrentHeld = null;
-    bool trig;
+    bool trig = false;
 
     SteamVR_TrackedObject trackedObj;
     FixedJoint joint;
@@ -16,13 +18,22 @@ public class HandHandler : MonoBehaviour
     void Awake()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
+        attachPoint = attachObject.GetComponent<Rigidbody>();
+        
+        
     }
 
     void FixedUpdate()
     {
+        //turns on hand models if they are off
+        if (attachObject.activeSelf == false){
+            attachObject.SetActive(true);
+        }
 
+        //gets the controllers to be checked for button input
         var device = SteamVR_Controller.Input((int)trackedObj.index);
 
+        //checks controllers for input
         if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)){
             trig = true;
         }
@@ -30,23 +41,53 @@ public class HandHandler : MonoBehaviour
         {
             trig = false;
             CurrentHeld = null;
+            //turns hand model back on
+            StartCoroutine(handShow());
         }
+        
 
-
+        //if object isnt being held and the trigger is being held
         if (joint == null && trig)
         {
-            var go = CurrentHeld;
-            go.transform.position = attachPoint.transform.position;
 
-            joint = go.AddComponent<FixedJoint>();
-            joint.connectedBody = attachPoint;
+           // GameObject go = CurrentHeld;
+            if (CurrentHeld != null)
+            {
+                handModel.SetActive(false);
+                //for objects that arent attached to the enviroment
+                if (CurrentHeld.tag == "Throwable")
+                {
+
+                    //(TEST BELOW FOR RESULTS)
+                    //go.transform.position = attachPoint.transform.position;
+
+                    joint = CurrentHeld.AddComponent<FixedJoint>();
+                    joint.connectedBody = attachPoint;
+                }
+
+                //for object that are attached to the enviroment
+                else if (CurrentHeld.tag == "moveable")
+                {
+
+                    Rigidbody puller = CurrentHeld.GetComponent<Rigidbody>();
+                    Vector3 pullPos = CurrentHeld.transform.position;
+                    Vector3 aimPos = attachObject.transform.position;
+
+                    puller.AddForce((aimPos - pullPos) * pullPower);
+
+                }
+            }
         }
+        //if object is being held that isnt attached to the enviroment
+        //and the trigger isnt being held
         else if (joint != null && !trig)
         {
-            var go = joint.gameObject;
-            var rigidbody = go.GetComponent<Rigidbody>();
+            
+            GameObject go = joint.gameObject;
+            Rigidbody rigidbody = go.GetComponent<Rigidbody>();
             Object.DestroyImmediate(joint);
             joint = null;
+            CurrentHeld = null;
             
 
             // We should probably apply the offset between trackedObj.transform.position
@@ -54,6 +95,8 @@ public class HandHandler : MonoBehaviour
             // location, however, we would then want to predict ahead the visual representation
             // by the same amount we are predicting our render poses.
 
+
+            //handles physics on objects being let go of
             var origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
             if (origin != null)
             {
@@ -68,21 +111,31 @@ public class HandHandler : MonoBehaviour
 
             rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
         }
+
+        print("TRIG " + trig + " " +gameObject.name);
     }
 
+    
     void OnTriggerStay(Collider col)
     {
-        if(col.tag == "Throwable" && CurrentHeld == null && trig)
+        //if object can be held/moved and no object is currently being held and the trigger is held
+        if(col.tag == "Throwable" || col.tag == "moveable" && CurrentHeld == null && trig)
         {
+            //turns hand off
+            //handModel.SetActive(false);
+
             CurrentHeld = col.gameObject;
         }
+        
     }
 
-    //void OnTriggerExit(Collider col)
-    //{
-    //    if (!trig)
-    //    {
-    //        CurrentHeld = null;
-    //    }
-    //}
+   IEnumerator handShow()
+    {
+        //when object is let go of this waits .1 seconds and then turns hand back on
+        //THIS SHOULD BE BETTER
+        //POSSIBLY USED OnTriggerExit INSTEAD
+        yield return new WaitForSeconds(0.1f);
+        handModel.SetActive(true);
+        CurrentHeld = null;
+    }
 }
